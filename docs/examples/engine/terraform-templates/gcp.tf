db3820204f8d5abdd36ca950cc646cb24c4fc5b9
Jesse Alford <jalford@pivotal.io>
2020-02-05 14:37:52 -0700

    use terraform template for GCP in pipeline
    Signed-off-by: JT Archie <jarchie@pivotal.io>
    Co-authored-by: JT Archie <jarchie@pivotal.io>

---
+++ docs/examples/engine/terraform-templates/gcp.tf
@@ -0,0 +1,63 @@
+resource "google_dns_record_set" "concourse" {
+  name = "ci.${var.environment_name}.${data.google_dns_managed_zone.hosted-zone.dns_name}"
+  type = "A"
+  ttl  = 60
+
+  managed_zone = var.hosted_zone
+
+  rrdatas = [google_compute_address.concourse.address]
+}
+
+//create a load balancer for concourse
+resource "google_compute_address" "concourse" {
+  name = "${var.environment_name}-concourse"
+}
+
+resource "google_compute_firewall" "concourse" {
+  allow {
+    ports    = ["443", "2222", "8844", "8443"]
+    protocol = "tcp"
+  }
+
+  direction     = "INGRESS"
+  name          = "${var.environment_name}-concourse-open"
+  network       = google_compute_network.network.self_link
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["concourse"]
+}
+
+resource "google_compute_forwarding_rule" "concourse_credhub" {
+  ip_address  = google_compute_address.concourse.address
+  ip_protocol = "TCP"
+  name        = "${var.environment_name}-concourse-credhub"
+  port_range  = "8844-8844"
+  target      = google_compute_target_pool.concourse_target_pool.self_link
+}
+
+resource "google_compute_forwarding_rule" "concourse_ssh" {
+  ip_address  = google_compute_address.concourse.address
+  ip_protocol = "TCP"
+  name        = "${var.environment_name}-concourse-ssh"
+  port_range  = "2222-2222"
+  target      = google_compute_target_pool.concourse_target_pool.self_link
+}
+
+resource "google_compute_forwarding_rule" "concourse_tcp" {
+  ip_address  = google_compute_address.concourse.address
+  ip_protocol = "TCP"
+  name        = "${var.environment_name}-concourse-tcp"
+  port_range  = "443-443"
+  target      = google_compute_target_pool.concourse_target_pool.self_link
+}
+
+resource "google_compute_forwarding_rule" "concourse_uaa" {
+  ip_address  = google_compute_address.concourse.address
+  ip_protocol = "TCP"
+  name        = "${var.environment_name}-concourse-uaa"
+  port_range  = "8443-8443"
+  target      = google_compute_target_pool.concourse_target_pool.self_link
+}
+
+resource "google_compute_target_pool" "concourse_target_pool" {
+  name = "${var.environment_name}-concourse"
+}
