e98270f338413ec0639304cf0c193cb3f59dbcf2
Ramkumar Vengadakrishnan <ramkumarv@vmware.com>
2023-01-24 11:11:38 -0600

    WIP: Minor refactors
    Renamed the task output and use environment variable in task script
    
    Authored-by: Ramkumar Vengadakrishnan <ramkumarv@vmware.com>

--- tasks/generate-certificate-authority.sh
+++ tasks/generate-certificate-authority.sh
@@ -5,7 +5,7 @@ cat /var/version && echo ""
 set -eux
 
 om --env env/"${ENV_FILE}" generate-certificate-authority --json > /tmp/CA.json
-om interpolate -c /tmp/CA --path /guid > ca-guid
+om interpolate -c /tmp/CA --path /guid > new-ca-guid
     # | jq .guid > new-ca/guid
     # | awk 'NR==4 { print $2 }' > new-ca/guid
     # | grep 'BEGIN CERTIFICATE' | awk '{ print $2 }' > new-ca/guid
