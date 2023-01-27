5ad89a9d29c53ac43059a198bde4504f4b6f6bd9
David Timm <dtimm@vmware.com>
2023-01-27 14:01:51 -0700

    docs: make script references unique

--- tasks/clean-certificates.sh
+++ tasks/clean-certificates.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# code_snippet clean-certificates start bash
+# code_snippet clean-certificates-script start bash
 
 cat /var/version && echo ""
 set -eux
@@ -7,4 +7,4 @@ set -eux
 local inactive_ca_guid="$(om --env env/"${ENV_FILE}"  certificate-authorities -- -f json | jq -r '.[] | select(.active == false).guid')"
 
 om --env env/"${ENV_FILE}" delete-certificate-authority --id "$inactive_ca_guid"
-# code_snippet clean-certificates end
+# code_snippet clean-certificates-script end
--- tasks/generate-certificate-authority.sh
+++ tasks/generate-certificate-authority.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# code_snippet generate-certificate-authority start bash
+# code_snippet generate-certificate-authority-script start bash
 
 cat /var/version && echo ""
 set -eux
 
 om --env env/"${ENV_FILE}" generate-certificate-authority --format json > CA.json
 om interpolate -c CA.json --path /guid > new-ca/guid
-# code_snippet generate-certificate-authority end
+# code_snippet generate-certificate-authority-script end
