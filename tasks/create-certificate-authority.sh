baa380947660c8624d13f69b2752f13db3d6e2ce
David Timm <dtimm@vmware.com>
2023-01-31 15:44:46 -0600

    refactor: Updated 3 tasks to match updated om cli
    - activate-certificate-authority now does not need CA GUID
    - create-certificate-authority and generate-certificate-authority
    no longer need to pass the GUID of the created CA
    
    Co-authored-by: David Timm <dtimm@vmware.com>
    Co-authored-by: Ramkumar Vengadakrishnan <ramkumarv@vmware.com>

--- tasks/create-certificate-authority.sh
+++ tasks/create-certificate-authority.sh
@@ -7,6 +7,5 @@ set -eux
 om --env env/"${ENV_FILE}" create-certificate-authority \
    --format json \
    --certificate-pem "$(<certs/certificate.pem)" \
-   --private-key-pem "$(<certs/privatekey.pem)" > CA.json
-om interpolate -c CA.json --path /guid > new-ca/guid
+   --private-key-pem "$(<certs/privatekey.pem)"
 # code_snippet create-certificate-authority-script end
