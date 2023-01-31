d81f57006667a003a18f09de266c476b636c8b56
David Timm <dtimm@vmware.com>
2023-01-31 16:24:59 -0600

    refactor: Composed create/generate CA to a single task
    - Delete generate and create certificate authority tasks
    - Replace with a single composite call that creates CA based
    on the inputs
    
    Co-authored-by: David Timm <dtimm@vmware.com>
    Co-authored-by: Ramkumar Vengadakrishnan <ramkumarv@vmware.com>

--- tasks/generate-certificate-authority.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# code_snippet generate-certificate-authority-script start bash
-
-cat /var/version && echo ""
-set -eux
-
-om --env env/"${ENV_FILE}" generate-certificate-authority
-# code_snippet generate-certificate-authority-script end
