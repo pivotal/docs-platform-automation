9a9ee09ec0b5e67bf17c446ca1cd0b2848862109
David Timm <dtimm@vmware.com>
2023-02-03 09:56:49 -0700

    feat: allow selective deploy configuration in apply-changes
    Co-authored-by: Ramkumar Vengadakrishnan <ramkumarv@vmware.com>

--- tasks/apply-changes.sh
+++ tasks/apply-changes.sh
@@ -18,6 +18,13 @@ if [ -n "${ERRAND_CONFIG_FILE}" ]; then
   flags+=("--config" "${ERRAND_CONFIG_FILE}")
 fi
 
+if [ -n "${SELECTIVE_DEPLOY_PRODUCTS}" ]; then
+  # convert comma-separated variable into bash-native space-separated
+  for PRODUCT in ${SELECTIVE_DEPLOY_PRODUCTS//,/ }; do
+    flags+=("--product-name" "${PRODUCT}")
+  done
+fi
+
 # ${flags[@] needs to be globbed to pass through properly
 # shellcheck disable=SC2068
 om --env env/"${ENV_FILE}" apply-changes \
