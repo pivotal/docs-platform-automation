8e0caec2d503d1f42efd559b0c2dfc266c194271
Anita Flegg <aflegg@vmware.com>
2024-01-03 09:06:20 -0800

    update code snippets in task reference

--- tasks/delete-installation.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# code_snippet delete-installation-script start bash
-
-cat /var/version && echo ""
-set -eux
-om --env env/"${ENV_FILE}" delete-installation --force
-# code_snippet delete-installation-script end
