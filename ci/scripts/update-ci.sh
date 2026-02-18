79893f4209e89abe2db9d6f5ff0229f8dde0aea3
ichandrabhatta <indira.chandrabhatta@broadcom.com>
2026-02-17 20:02:05 -0800

    Chore

--- ci/scripts/update-ci.sh
+++ ci/scripts/update-ci.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-TARGET=${1:-platform-automation}
+TARGET=tpe
 
 which ytt || (
   echo "This requires ytt to be installed"
@@ -18,19 +18,19 @@ which fly || (
 echo "Setting CI pipeline..."
 
 fly -t $TARGET sp -p platform-automation-ci -c <(ytt -f $WORKING_DIR/../ci/) \
-  --check-creds
+ -v eos_date=03/20/2027
 
-echo "Setting support pipeline..."
+# echo "Setting support pipeline..."
 
-fly -t $TARGET sp -p support-pipeline -c <(ytt -f $WORKING_DIR/../opsman-support) \
-  --check-creds
-
-# echo "Setting Docs CI pipeline on TPE CI..."
-# 
-# fly -t $TARGET sp -p platform-automation-docs -c <(ytt -f "$WORKING_DIR/../docs/") \
+# fly -t $TARGET sp -p support-pipeline -c <(ytt -f $WORKING_DIR/../opsman-support) \
 #   --check-creds
 
-echo "Setting OSSPI pipeline on TPE CI..."
+# # echo "Setting Docs CI pipeline on TPE CI..."
+# # 
+# # fly -t $TARGET sp -p platform-automation-docs -c <(ytt -f "$WORKING_DIR/../docs/") \
+# #   --check-creds
 
-fly -t $TARGET sp -p osspi-platform-automation -c <(ytt -f "$WORKING_DIR/../osspi-tpe-ci/") \
-  --check-creds
+# echo "Setting OSSPI pipeline on TPE CI..."
+
+# fly -t $TARGET sp -p osspi-platform-automation -c <(ytt -f "$WORKING_DIR/../osspi-tpe-ci/") \
+#   --check-creds
