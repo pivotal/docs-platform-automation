76e7f1663fec9df74ceb915fcc9a5014809143e8
rjanakiraman <rjanakiraman@vmware.com>
2026-02-27 05:50:09 +0530

    TNZ-48870 correct plugin path for packer

--- tasks/build-windows-stemcell.sh
+++ tasks/build-windows-stemcell.sh
@@ -23,7 +23,7 @@ pushd $SCRIPT_DIR/windows-automation
 export PACKER_PLUGIN_PATH=/root/.packer.d/plugins
 
 mkdir -p ${PACKER_PLUGIN_PATH}/github.com/hashicorp/vsphere
-mv $PACKER_PLUGIN_PATH/acker-plugin-vsphere_v1.4.0_x5.0_linux_amd64 $PACKER_PLUGIN_PATH/github.com/hashicorp/vsphere/packer-plugin-vsphere_v1.4.0_x5.0_linux_amd64
+mv $PACKER_PLUGIN_PATH/packer-plugin-vsphere_v1.4.0_x5.0_linux_amd64 $PACKER_PLUGIN_PATH/github.com/hashicorp/vsphere/packer-plugin-vsphere_v1.4.0_x5.0_linux_amd64
 OLD_PWD=$OLDPWD
 
 # Generate variables file from Concourse task inputs
